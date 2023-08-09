5e188dfeef746476520059ee0a0c95622e7d72fd
wooooozin <wooooozin@gmail.com>
2023-08-09 21:45:05 +0900

    GET 복수형 모델로 반환하기 + Builder패턴 사용

--- sample1/src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
+++ sample1/src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ApiNoticeController {
@@ -14,13 +16,35 @@ public class ApiNoticeController {
 //        return "공지사항입니다.";
 //    }
 
+    //    @GetMapping("/api/notice")
+//    public NoticeModel notice() {
+//        NoticeModel noticeModel = new NoticeModel();
+//        noticeModel.setId(1);
+//        noticeModel.setTitle("공지사항 제목 입니다.");
+//        noticeModel.setContents("공지사항 내용 입니다.");
+//        noticeModel.setRegDate(LocalDateTime.now());
+//        return noticeModel;
+//    }
+
     @GetMapping("/api/notice")
-    public NoticeModel notice() {
-        NoticeModel noticeModel = new NoticeModel();
-        noticeModel.setId(1);
-        noticeModel.setTitle("공지사항 제목 입니다.");
-        noticeModel.setContents("공지사항 내용 입니다.");
-        noticeModel.setRegDate(LocalDateTime.now());
-        return noticeModel;
+    public List<NoticeModel> notice() {
+        List<NoticeModel> noticeModelList = new ArrayList<>();
+        NoticeModel noticeModel1 = new NoticeModel();
+        noticeModel1.setId(1);
+        noticeModel1.setTitle("공지사항 입니다.");
+        noticeModel1.setContents("공지사항 내용입니다.");
+        noticeModel1.setRegDate(LocalDateTime.of(2023, 1, 31, 0, 0));
+        noticeModelList.add(noticeModel1);
+
+        NoticeModel noticeModel2 = NoticeModel.builder()
+                .id(2)
+                .title("두번째 공지사항 입니다.")
+                .contents("두번째 공지사항 내용 입니다.")
+                .regDate(LocalDateTime.of(2023, 1, 31, 0, 0))
+                .build();
+        noticeModelList.add(noticeModel2);
+
+        return noticeModelList;
     }
+
 }
--- sample1/src/main/java/com/example/jpa/notice/model/NoticeModel.java
+++ sample1/src/main/java/com/example/jpa/notice/model/NoticeModel.java
@@ -1,9 +1,15 @@
 package com.example.jpa.notice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Data
 public class NoticeModel {
     // ID, 제목, 내용, 등록일(작성일)
