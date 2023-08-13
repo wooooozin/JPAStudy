9fb5d8ed213c1000d3d7095e369de203357300ac
wooooozin <wooooozin@gmail.com>
2023-08-14 00:04:11 +0900

    페이징 처리 및 정렬

--- sample1/src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
+++ sample1/src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
@@ -9,6 +9,9 @@ import com.example.jpa.notice.model.ResponseError;
 import com.example.jpa.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -270,5 +273,11 @@ public class ApiNoticeController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/api/notice/latest/{size}")
+    public Page<Notice> noticeLatest(@PathVariable int size) {
+        Page<Notice> notices =
+        noticeRepository.findAll(PageRequest.of(0, size, Sort.Direction.DESC, "regDate"));
+        return notices;
+    }
 
 }
