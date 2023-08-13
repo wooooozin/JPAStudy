3cfad6e66aae958ea3d92f38714a0493cb0a8a8b
wooooozin <wooooozin@gmail.com>
2023-08-13 23:56:42 +0900

    제목과 내용 입력 내용 제한 후 에러 발생시 예외처리

--- sample1/src/main/java/com/example/jpa/notice/model/NoticeInput.java
+++ sample1/src/main/java/com/example/jpa/notice/model/NoticeInput.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,8 +15,11 @@ import javax.validation.constraints.NotBlank;
 public class NoticeInput {
 
     // ID, 제목, 내용, 등록일(작성일)
+    @Size(min = 10, max = 100, message = "제목은 10~100자 사이의 값입ㅂ니다.")
     @NotBlank(message = "제목은 필수 항목 입니다.")
     private String title;
+
+    @Size(min = 50, max = 500, message = "내용은 50~500자 사이의 값입ㅂ니다.")
     @NotBlank(message = "내용은 필수 항목 입니다.")
     private String contents;
 }
