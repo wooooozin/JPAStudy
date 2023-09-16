458b42030291d8c932315143c8afadc0825b9e53
wooooozin <wooooozin@gmail.com>
2023-09-16 20:17:27 +0900

    board comment 엔티티 생성

---
+++ sample1/src/main/java/com/example/jpa/board/entity/BoardComment.java
@@ -0,0 +1,36 @@
+package com.example.jpa.board.entity;
+
+import com.example.jpa.user.entity.AppUser;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+public class BoardComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn
+    private AppUser user;
+
+    @ManyToOne
+    @JoinColumn
+    private Board board;
+
+    @Column
+    private String comments;
+
+    @Column
+    private LocalDateTime regDate;
+
+}
