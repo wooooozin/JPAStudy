6f3417d0acb25ea1b0b92aec973d338cffe6850f
wooooozin <wooooozin@gmail.com>
2023-09-28 17:53:56 +0900

    객체화 해서 Json 형태로 파싱하는 api

--- sample1/src/main/java/com/example/jpa/extra/ApiExtraController.java
+++ sample1/src/main/java/com/example/jpa/extra/ApiExtraController.java
@@ -1,11 +1,17 @@
 package com.example.jpa.extra;
 
+import com.example.jpa.common.model.ResponseResult;
+import com.example.jpa.extra.model.OpenApiResult;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,9 +22,7 @@ public class ApiExtraController {
 
 
     @GetMapping("/api/extra/pharmacy")
-    public String pharmacy(
-
-    ) {
+    public ResponseEntity<?> pharmacy() {
         String apiResult = "";
         String apiKey = "4ai%2FD6l7vfzncAjYnLRRlEgF2w3Oj4mT%2BBs6SJ96GZyL4Nj27cirw14KcDkSR6DddR6AbH%2B0VNWxl0ac8HbUbA%3D%3D";
         String url = "https://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?serviceKey=%s&pageNo=1&numOfRows=10";
@@ -28,10 +32,21 @@ public class ApiExtraController {
             HttpHeaders headers = new HttpHeaders();
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
             apiResult = restTemplate.getForObject(uri, String.class);
-            return apiResult;
+
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+        OpenApiResult jsonResult = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            jsonResult = objectMapper.readValue(apiResult, OpenApiResult.class);
 
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ResponseResult.success(jsonResult);
     }
 }
---
+++ sample1/src/main/java/com/example/jpa/extra/model/OpenApiResultResponseBodyItemsItem.java
@@ -0,0 +1,42 @@
+package com.example.jpa.extra.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class OpenApiResultResponseBodyItemsItem {
+    private String dutyAddr;
+    private String dutyInt;
+    private String dutyName;
+    private String dutyMapimg;
+    private String dutyTel1;
+    private String dutyTime1c;
+    private String dutyTime1s;
+    private String dutyTime2c;
+    private String dutyTime2s;
+    private String dutyTime3c;
+    private String dutyTime3s;
+    private String dutyTime4c;
+    private String dutyTime4s;
+    private String dutyTime5c;
+    private String dutyTime5s;
+    private String dutyTime6c;
+    private String dutyTime6s;
+    private String dutyTime7c;
+    private String dutyTime7s;
+    private String dutyTime8c;
+    private String dutyTime8s;
+    private String hpid;
+    private String postCdn1;
+    private String postCdn2;
+    private int rnum;
+    private double wgs84Lat;
+    private double wgs84Lon;
+}
+
